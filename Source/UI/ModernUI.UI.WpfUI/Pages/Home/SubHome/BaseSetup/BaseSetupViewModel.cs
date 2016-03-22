ef10eda0a4eb4da4cd1a491488b69e195e9a520e
Peter Kottas <petokottas@gmail.com>
2016-03-22 19:57:14 +0100

    Added missing files

---
+++ Source/UI/ModernUI.UI.WpfUI/Pages/Home/SubHome/BaseSetup/BaseSetupViewModel.cs
@@ -0,0 +1,52 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using NugetWorkflow.UI.WpfUI.Common.Interfaces;
+
+namespace NugetWorkflow.UI.WpfUI.Pages.Home.SubHome.BaseSetup
+{
+    public class BaseSetupViewModel : NotifyPropertyChanged, IView
+    {
+        private string basePath;
+
+        public RelayCommand ChooseBasePathCommand { get; set; }
+
+        public string BasePath
+        {
+            get
+            {
+                return basePath;
+            }
+            set
+            {
+                basePath = value;
+                OnPropertyChanged("BasePath");
+            }
+        }
+
+        public BaseSetupViewModel()
+        {
+            ChooseBasePathCommand = new RelayCommand(ChooseBasePathExecute);
+        }
+
+        private void ChooseBasePathExecute(object parameter)
+        {
+            var dlg = new CommonOpenFileDialog();
+            dlg.Title = "Choose base path for your nuget solutions";
+            dlg.IsFolderPicker = true;
+
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems = false;
+            dlg.EnsureFileExists = true;
+            dlg.EnsurePathExists = true;
+            dlg.EnsureReadOnly = false;
+            dlg.EnsureValidNames = true;
+            dlg.Multiselect = false;
+            dlg.ShowPlacesList = true;
+
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                BasePath = dlg.FileName;
+            }
+        }
+    }
+}
---
+++ Source/UI/ModernUI.UI.WpfUI/Pages/Home/SubHome/GitRepos/GitReposPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NugetWorkflow.UI.WpfUI.Pages;
+using NugetWorkflow.UI.WpfUI.Extensions;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Data;
+using NugetWorkflow.UI.WpfUI.Common.Base;
+using NugetWorkflow.UI.WpfUI.Utils;
+using NugetWorkflow.UI.WpfUI.Pages.Home.SubHome.GitRepos.Models;
+
+namespace NugetWorkflow.UI.WpfUI.Pages.Home.SubHome.GitRepos
+{
+    /// <summary>
+    /// Interaction logic for GitRepos.xaml
+    /// </summary>
+    public partial class GitReposPage : UserControl
+    {
+        private GitReposModel viewModel
+        {
+            get
+            {
+                return this.DataContext as GitReposModel;
+            }
+        }
+
+        public GitReposPage()
+        {
+            InitializeComponent();
+            //container = ((App)Application.Current).Container;
+            this.DataContext = ViewModelService.GetViewModel<GitReposModel>();
+        }
+    }
+}
