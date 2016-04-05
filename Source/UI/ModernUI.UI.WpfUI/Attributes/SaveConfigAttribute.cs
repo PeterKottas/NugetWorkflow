a00ff841227c9334ef00b506365c86f94c917174
Peter Kottas <petokottas@gmail.com>
2016-04-05 18:16:55 +0200

    Added SaveConfigAttribute

---
+++ Source/UI/ModernUI.UI.WpfUI/Attributes/SaveConfigAttribute.cs
@@ -0,0 +1,13 @@
+﻿using NugetWorkflow.UI.WpfUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetWorkflow.UI.WpfUI.Attributes
+{
+    public class SaveConfigAttribute : Attribute
+    {
+    }
+}
